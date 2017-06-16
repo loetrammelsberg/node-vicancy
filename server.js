74afbdfc74f826c4959fd14162caa78bf7506236
weirong <weirong@vicancy.com>
2017-06-16 11:16:06 +0200

    testing

--- server.js
+++ server.js
@@ -32,10 +32,10 @@ var userUrl = '';
 // test route to make sure everything is working (accessed at POST http://localhost:8080/api)
 router.post('/', function (req, res) {
     userUrl = path.join('https://dashboard-staging.hrofficelabs.com/api/external/credentials?token=' + req.body.token);
-    res.send(userUrl);
+    res.end();
 });
 
-
+console.log(userUrl);
 
 // more routes for our API will happen here
 
