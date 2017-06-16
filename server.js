2192df11347f0df1e621b64738f2834c287f3b55
weirong <weirong@vicancy.com>
2017-06-16 15:36:39 +0200

    testing

--- server.js
+++ server.js
@@ -39,9 +39,38 @@ router.post('/', function (req, res) {
 
 });
 router.get('/api', function (req, res) {
-    request.get(userUrl, options, function (err, res, body) {
-        //TODO Do something with response
-        res.send('hello');
+    http.get(userUrl, (res) => {
+        const { statusCode } = res;
+        const contentType = res.headers['content-type'];
+
+        let error;
+        if (statusCode !== 200) {
+            error = new Error('Request Failed.\n' +
+                `Status Code: ${statusCode}`);
+        } else if (!/^application\/json/.test(contentType)) {
+            error = new Error('Invalid content-type.\n' +
+                `Expected application/json but received ${contentType}`);
+        }
+        if (error) {
+            console.error(error.message);
+            // consume response data to free up memory
+            res.resume();
+            return;
+        }
+
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+            try {
+                const parsedData = JSON.parse(rawData);
+                console.log(parsedData);
+            } catch (e) {
+                console.error(e.message);
+            }
+        });
+    }).on('error', (e) => {
+        console.error(`Got error: ${e.message}`);
     });
 });
 
