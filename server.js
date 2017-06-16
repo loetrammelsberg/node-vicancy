3365bda147687c753cdc73c328a2e9b87acf15ef
weirong <weirong@vicancy.com>
2017-06-16 17:07:43 +0200

    testing

--- server.js
+++ server.js
@@ -49,16 +49,32 @@ router.post('/', function (req, res) {
 app.use('/', router);
 
 app.get('/api', function (req, res) {
-    var options = {
-        uri: userUrl ,
-        method: 'GET'
-    }
-    request(options, function (error, response, body) {
-        var data = {
-            userName: '',
-            productSetting: ''
-        };
-        res.send(data);
+    // var options = {
+    //     uri: userUrl ,
+    //     method: 'GET'
+    // }
+    // request(options, function (error, response, body) {
+    //     var data = {
+    //         userName: '',
+    //         productSetting: ''
+    //     };
+    //     res.send(data);
+    // });
+    http.get(options, function (http_res) {
+        // initialize the container for our data
+        var data = "";
+
+        // this event fires many times, each time collecting another piece of the response
+        http_res.on("data", function (chunk) {
+            // append this chunk to our growing `data` var
+            data += chunk;
+        });
+
+        // this event fires *one* time, after all the `data` events/chunks have been gathered
+        http_res.on("end", function () {
+            // you can use res.send instead of console.log to output via express
+            console.log(data);
+        });
     });
 });
 
