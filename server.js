68fcce1f95ce41fe0f21051840a7da77f7d188d9
weirong <weirong@vicancy.com>
2017-06-19 11:41:11 +0200

    testing

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/api', function (req, res) {
             }
 
         }
-        request.get(userUrl, function (error, response, body) {
+        request.get(options, function (error, response, body) {
             console.log('error:', error); // Print the error if one occurred 
             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
             console.log('body:', body); // Print the HTML for the Google homepage. 
