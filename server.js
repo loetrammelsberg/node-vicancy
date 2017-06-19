5ceb07d9abf423ee150c70dba662f92c11e8fe20
weirong <weirong@vicancy.com>
2017-06-19 11:15:01 +0200

    testing

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ router.post('/', function (req, res) {
 
 var request = require('request');
 if (flag) {
+    console.log('hello');
     request(userUrl, function (error, response, body) {
         console.log('error:', error); // Print the error if one occurred 
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
