6962b2fc8855153ac489294e13e99c1209100b63
weirong <weirong@vicancy.com>
2017-06-20 11:23:51 +0200

    testing

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ router.post('/', function (req, res) {
 
         }
 
-        request.get(options, function (error, response, body) {
+        request(options, function (error, response, body) {
             console.log('error:', error); // Print the error if one occurred 
             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
             console.log('body:', body);
