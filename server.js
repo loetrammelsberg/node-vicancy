6ed21ced93791d71a26a002eb47bb95fe9a75c0b
weirong <weirong@vicancy.com>
2017-06-23 09:47:54 +0200

    testing

--- server.js
+++ server.js
@@ -166,7 +166,7 @@ app.get('/', function (req, res) {
     //__dirname : It will resolve to your project folder.
 });
 
-app.get('/', function (req, res) {
+app.get('/api', function (req, res) {
     res.sendFile(path.join(__dirname + '/app.ejs'));
     //__dirname : It will resolve to your project folder.
 });
