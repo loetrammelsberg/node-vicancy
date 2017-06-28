017a9cd956c5d7672e961bd5d42c8f750b054a44
weirong <weirong@vicancy.com>
2017-06-28 14:37:52 +0200

    testing

--- server.js
+++ server.js
@@ -125,9 +125,8 @@ function selectUser(username, client) {
         username = 'testing';
     }
     client.query("SELECT clients.external_id,clients.name,clients.email,clients.language,resellers.token FROM resellers INNER JOIN clients on resellers.id = clients.reseller_id WHERE resellers.name = '" + reseller + "' AND clients.name = '" + username + "'", function (err, result) {
-        if (typeof result.rows[0] == 'undefined') {
-            inserUser(username, reseller, err, client);
-        } else {
+        if (typeof result.rows[0] != 'undefined') {
+            rowResult = result.rows[0];
             id = result.rows[0].external_id;
             name = result.rows[0].name;
             email = result.rows[0].email;
@@ -137,7 +136,11 @@ function selectUser(username, client) {
                 language = 'nl';
             }
         }
-
+        pg.end();
+        
+        if (rowResultid == '') {
+            insertUser(username, reseller);
+        }
 
         console.log(id);
         console.log(name);
@@ -149,39 +152,44 @@ function selectUser(username, client) {
     return rowResult;
 }
 
-function inserUser(username,reseller, err, client) {
-    var resellerToken = '';
-    client.query("SELECT resellers.token FROM resellers WHERE resellers.name = ('" + reseller + "')"), function (err, result) {
-        console.log(result.rows[0] + "INSERT USER!");
-        resellerToken = result.rows[0].token;
-        
-    };
-    
-    var options = {
-        url: 'http://app.vicancy.com/api/v1/client/auth',
-        method: "POST",
-        qs: {
-            api_token: resellerToken,
-            client: {
-                id: '1000',
-                name: username,
-                email: '',
-                language: 'nl'
-            }
-        },
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        }
-    }
+function insertUser(username, reseller) {
 
-    request.post(options, function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred 
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-        console.log('body:', body);
-        console.log('hello');
+    var resellerToken = '';
+    pg.connect('postgres://qsxeiddqmzyjtl:Yr6gsDFcIw3QIlJH9tVSJ7f9xt@ec2-54-246-96-114.eu-west-1.compute.amazonaws.com:5432/d1fu206la3ndei', function (err, client) {
+        client.query("SELECT resellers.token FROM resellers WHERE resellers.name = ('" + reseller + "')"), function (err, result) {
+            if (err) throw err;
+            console.log(result.rows[0] + "INSERT USER!");
+            resellerToken = result.rows[0].token;
+
+        };
     });
-}
+        var options = {
+            url: 'http://app.vicancy.com/api/v1/client/auth',
+            method: "POST",
+            qs: {
+                api_token: resellerToken,
+                client: {
+                    id: '1000',
+                    name: username,
+                    email: '',
+                    language: 'nl'
+                }
+            },
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }
+        }
+
+        request.post(options, function (error, response, body) {
+            console.log('error:', error); // Print the error if one occurred 
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+            console.log('body:', body);
+            console.log('hello');
+        });
+
+        pg.end();
+    }
 
 
 // more routes for our API will happen here
@@ -189,38 +197,38 @@ function inserUser(username,reseller, err, client) {
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /
 app.use('/', router);
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/widget.ejs'));
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname + '/widget.ejs'));
 
-    //__dirname : It will resolve to your project folder.
-});
+        //__dirname : It will resolve to your project folder.
+    });
 
-app.get('/app', function (req, res) {
-    res.render(path.join(__dirname + '/View/app.ejs'));
+    app.get('/app', function (req, res) {
+        res.render(path.join(__dirname + '/View/app.ejs'));
 
-    //__dirname : It will resolve to your project folder.
-});
+        //__dirname : It will resolve to your project folder.
+    });
 
-app.get('/api', function (req, res) {
-    console.log(id);
-    console.log(name);
-    console.log(vToken);
-    console.log(language);
-    res.render('widget.ejs', { id: id, name: name, vToken: vToken, email: email, language: language });
-});
+    app.get('/api', function (req, res) {
+        console.log(id);
+        console.log(name);
+        console.log(vToken);
+        console.log(language);
+        res.render('widget.ejs', { id: id, name: name, vToken: vToken, email: email, language: language });
+    });
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname + '/index.html'));
 
-    //__dirname : It will resolve to your project folder.
-});
+        //__dirname : It will resolve to your project folder.
+    });
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/style.css'));
-    //__dirname : It will resolve to your project folder.
-});
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname + '/style.css'));
+        //__dirname : It will resolve to your project folder.
+    });
 
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+    // START THE SERVER
+    // =============================================================================
+    app.listen(port);
+    console.log('Magic happens on port ' + port);
