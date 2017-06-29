7e06c8d2523e1c806e27983ef42fe74e0162f18d
weirong <weirong@vicancy.com>
2017-06-29 16:26:44 +0200

    testing

--- server.js
+++ server.js
@@ -128,16 +128,15 @@ function selectUser(username, reseller) {
         console.log('Connected to postgres! Getting schemas...');
         client.query("SELECT clients.external_id,clients.name,clients.email,clients.language,resellers.token FROM resellers INNER JOIN clients on resellers.id = clients.reseller_id WHERE resellers.name = '" + reseller + "' AND clients.name = '" + username + "'", function (err, result) {
             console.log(result.rows[0] != null);
-            if (result.rows[0] != null) {
-                rowResult = result.rows[0].external_id;
-                id = result.rows[0].external_id;
-                name = result.rows[0].name;
-                email = result.rows[0].email;
-                vToken = result.rows[0].token;
-                language = result.rows[0].language;
-                if (language == null) {
-                    language = 'nl';
-                }
+
+            id = result.rows[0].external_id;
+            name = result.rows[0].name;
+            email = result.rows[0].email;
+            vToken = result.rows[0].token;
+            language = result.rows[0].language;
+            if (language == null) {
+                language = 'nl';
+
             }
         });
     });
