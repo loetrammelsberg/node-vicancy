63b3b461947f5555b62c1658188299fce1c8be7b
weirong <weirong@vicancy.com>
2017-07-05 13:13:52 +0200

    testing

--- server.js
+++ server.js
@@ -179,11 +179,12 @@ function generateToken(callback) {
         for (var i = 0; i < 8; i++) {
             text += randomItem(result)
         }
-
+        console.log(text);
         pg.connect(con, function (err, client, done) {
             if (err) throw err;
             console.log('Connected to postgres! Getting schemas...!');
             client.query("SELECT clients.external_id FROM clients where clients.external_id = '" + text + "';", function (err, result) {
+
                 console.log(result.rows.length);
                 if (result.rows.length == 0) {
                     check = false;
