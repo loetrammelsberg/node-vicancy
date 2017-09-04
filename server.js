989ecf7176750a9cc1b40bea6d9bfc0315da28e8
weirongrong <weirong@vicancy.com>
2017-09-04 19:54:19 +0800

    testing

--- server.js
+++ server.js
@@ -57,6 +57,7 @@ var username = '';
  * @desc expose web service via POST request to HRAppstore to recieve token from it (Take note: Token can only be use once!)
  */
 router.post('/', function (req, res) {
+    console.log(req);
     var token = req.body.token; //get token from request body
     var result = '';
     Sync(function () { //Synchronise the code. 
@@ -282,8 +283,6 @@ app.get('/api', function (req, res) {//this where we show the vicancy logo for t
     console.log(name);
     console.log(vToken);
     console.log(language);
-    var id = req.query;
-    console.log(id);
     res.render('widget.ejs', { id: id, name: name, vToken: vToken, email: email, language: language });
 });
 
